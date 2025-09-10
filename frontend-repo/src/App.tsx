7a999ef2852dbb233622337979eb3a707defdb73
DONGGYU <rlaehdrb1993@naver.com>
2025-09-10 23:37:02 +0900

    [FE] feature: 로그인 페이지 구현

--- frontend-repo/src/App.tsx
+++ frontend-repo/src/App.tsx
@@ -1,4 +1,29 @@
-const App = () => {
-  return <div>App</div>
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+import { LoginPage } from '@/features/auth'
+import GlobalStyles from '@/styles/GlobalStyles'
+
+export const App = () => {
+  return (
+    <>
+      {/* 전역 스타일 적용 */}
+      <GlobalStyles />
+
+      <Router>
+        <Routes>
+          {/* 기본 경로를 로그인 페이지로 리다이렉트 */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* 로그인 페이지 라우트 */}
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 export default App
---
+++ frontend-repo/src/features/auth/index.ts
@@ -0,0 +1,10 @@
+// Web exports
+export { LoginPage, LoginForm, AppHeader } from './web'
+
+// Types exports
+export type {
+  LoginRequest,
+  LoginResponse,
+  LoginFormData,
+  AuthError,
+} from './types'
